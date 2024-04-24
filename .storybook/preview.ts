dc4deccdb881c28dc48f1ec70c2363d5f6737eb4
João Domingos Pinheiro Neto <56659658+joaonetogit@users.noreply.github.com>
2024-04-24 00:02:53 -0300

    feat: themes dark/light on storybook preview, types on story components, adjusts require tailwind

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,6 @@
-import type { Preview } from "@storybook/react";
+import { withThemeByClassName } from "@storybook/addon-themes";
+import type { Preview, ReactRenderer } from "@storybook/react";
+import "../src/index.css";
 
 const preview: Preview = {
   parameters: {
@@ -9,6 +11,15 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [
+    withThemeByClassName<ReactRenderer>({
+      themes: {
+        light: "",
+        dark: "dark",
+      },
+      defaultTheme: "light",
+    }),
+  ],
 };
 
 export default preview;
--- src/stories/components/Button.stories.ts
+++ src/stories/components/Button.stories.ts
@@ -1,15 +1,27 @@
 import { Button } from "@/components/Button";
-import type { Meta } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react";
 
 const meta = {
   component: Button,
   args: {
     children: "Button",
-  }
+  },
 } satisfies Meta<typeof Button>;
 
 export default meta;
 
-export const Default = {};
+type Story = StoryObj<typeof meta>;
 
-export const Primary = {};
+export const Primary: Story = {
+  args: {
+    variant: "default",
+    children: "Primary Button",
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    variant: "secondary",
+    children: "Secondary Button",
+  },
+};
