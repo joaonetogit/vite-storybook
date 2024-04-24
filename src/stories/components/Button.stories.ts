4161a86a2706d126bd3055b4c89777262c9c21e2
João Domingos Pinheiro Neto <56659658+joaonetogit@users.noreply.github.com>
2024-04-24 13:28:02 -0300

    feat: theming, logo, button story

--- src/stories/components/Button.stories.ts
+++ src/stories/components/Button.stories.ts
@@ -2,9 +2,52 @@ import { Button } from "@/components/Button";
 import type { Meta, StoryObj } from "@storybook/react";
 
 const meta = {
+  title: "Components/Button",
   component: Button,
+  tags: ["autodocs"],
   args: {
     children: "Button",
+    disabled: false,
+    size: "default",
+    variant: "default",
+  },
+  argTypes: {
+    asChild: {
+      table: {
+        disable: true,
+      },
+    },
+    disabled: {
+      control: "boolean",
+    },
+    size: {
+      options: ["default", "sm", "md", "lg", "icon"],
+      control: "select",
+      table: {
+        type: {
+          summary: "enum (string)",
+          detail: '"default" | "sm" | "md" | "lg" | "icon"',
+        },
+      },
+    },
+    variant: {
+      options: [
+        "default",
+        "destructive",
+        "secondary",
+        "outline",
+        "ghost",
+        "link",
+      ],
+      control: "select",
+      table: {
+        type: {
+          summary: "enum (string)",
+          detail:
+            '"default" | "destructive" | "secondary" | "outline" | "ghost" | "link"',
+        },
+      },
+    },
   },
 } satisfies Meta<typeof Button>;
 
